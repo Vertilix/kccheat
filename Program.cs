275b11da3c2ed5c6ad8e7d96ddbca990344eaf50
VNS <96177454+Vertilix@users.noreply.github.com>
2022-09-23 09:02:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+namespace PS
+{
+    class Program
+    {
+        // dotnet publish --output "/home/mike/Desktop/Code/C#/kc/bin/Release/net6.0/" --runtime win-x64 --configuration Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        public static void Main(string[] args)
+        {
+            // bool running = false;
+            // bool check = false;
+            // ConsoleKeyInfo cki;
+
+            // Console.WriteLine("Druk op de PageUp knop om de script te stoppen/starten.");
+
+            while (true)
+            {
+                // cki = Console.ReadKey(true);
+                // Console.WriteLine(running);
+                // bool keypress = cki.Key == ConsoleKey.PageUp;
+                //     if (keypress)
+                //     {
+                //         check = true;
+                //     }
+
+                //     if (keypress == false)
+                //     {
+                //         if (check && running)
+                //         {
+                //             check = false;
+                //             running = false;
+                //         }
+                //         if (check == false && running == false)
+                //         {
+                //             check = false;
+                //             running = true;
+                //         }
+                //     }
+
+                //     if (running)
+                //     {
+                        System.Threading.Thread.Sleep(300);
+                        Console.WriteLine(GetColorAt(23,23));
+                    // }
+
+
+            }
+            Console.ReadLine();
+        }
+
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int) GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+
+            Color pixel = Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+            byte green = pixel.G;
+            byte blue = pixel.B;
+            byte red = pixel.R;
+            if (red == 255 && blue == 0 && green == 0){
+                Console.WriteLine("Right Click!");
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            }
+            return pixel;
+        }
+    }
+}
